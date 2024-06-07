430ba7b7f9733f3e843fd5c79cf2dc5ca4bdaea7
Saddam Ansari <saddammalik498@gmail.com>
2024-06-07 23:23:40 +0530

    fix

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const api = require("./routes/api");
-const app = express();
 const cors = require("cors");
+const api = require("./routes/api");
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
+// Parse and log whitelist domains
 let whitelist;
 try {
   whitelist = JSON.parse(process.env.WHITELIST_DOMAINS);
+  console.log("Whitelist domains:", whitelist);
 } catch (error) {
   console.error("Error parsing WHITELIST_DOMAINS:", error);
   process.exit(1);
 }
 
+// CORS configuration with detailed logging
 var corsOptions = {
   origin: function (origin, callback) {
-    if (process.env.NODE_ENV == "development") {
+    console.log("Incoming request from origin:", origin);
+    if (!origin) {
+      return callback(null, true);
+    }
+    if (process.env.NODE_ENV === "development") {
       callback(null, true);
     } else {
       if (whitelist?.indexOf(origin) !== -1) {
@@ -31,21 +39,21 @@ var corsOptions = {
     }
   }
 };
+
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Enable preflight requests for all routes
+
 app.use((err, req, res, next) => {
   if (err) {
     return res.status(403).json({ error: err.message });
   }
   next();
 });
-app.use((req, res, next) => {
-  res.set({
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "*",
-    "Access-Control-Allow-Headers":
-      "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'"
-  });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
@@ -55,7 +63,8 @@ const init = async () => {
 
   let port = process.env.PORT || 8080;
   app.listen(port, () => {
-    console.log(`Sever Running at port Number ${port}`);
+    console.log(`Server running at port number ${port}`);
   });
 };
+
 init();
